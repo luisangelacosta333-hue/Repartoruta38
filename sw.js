d8298115829e84eb9ce1fa04ca838568b68f88c8
luisangelacosta333-hue <luisangelacosta333@gmail.com>
2026-07-31 13:43:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
+const CACHE_NAME = 'ruta38-v1';
+
+// Se instala el Service Worker
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
+    self.skipWaiting(); // Fuerza la activación inmediata
 });
 
+// Se activa el Service Worker
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+    event.waitUntil(clients.claim()); // Toma el control de la página rápido
 });
 
+// ESTO ES OBLIGATORIO PARA QUE GOOGLE CHROME DEJE INSTALAR LA APP
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            // Si no hay internet, no hace nada grave, pero cumple el requisito.
+            return new Response("Estás sin conexión a internet.");
+        })
+    );
 });
 
